2206e35fc61777442fc157d867f12f2c131b9730
KohlRaphael <raphael.kohl@charite.de>
2024-02-23 16:48:17 +0100

    add custom num layer with german symbols

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,14 +9,14 @@
 //U_LT(U_BUTTON, DE_Y), U_MT(RALT, X),  &kp C,              &kp V,              &kp B,              &kp N,            &kp M,              &kp COMMA,          U_MT(RALT, DOT),  U_LT(U_BUTTON, SLASH),  \
 //U_NP,                 U_NP,           U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPC),   U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET), U_LT(U_NUM, BSPC),  U_LT(U_FUN, DEL),   U_NP,             U_NP
 
-//#define MIRYOKU_LAYER_BASE \
-&kp P,                &kp O,          &kp DE_MINS,        &kp DE_COMM,        &kp Q,            &kp G,            &kp C,              &kp L,              &kp M,            &kp F,                  \
-U_MT(LGUI, H),        U_MT(LALT, I),  U_MT(LCTRL, E),     U_MT(LSHFT, A),     &kp O,              &kp D,            U_MT(LSHFT, T),     U_MT(LCTRL, R),     U_MT(LALT, N),    U_MT(LGUI, S),        \
-U_LT(U_BUTTON, K),    U_MT(RALT, Y),  &kp DE_DOT,         &kp DE_QUOT,        &kp X,              &kp J,            &kp V,              &kp W,              U_MT(RALT, B),    U_LT(U_BUTTON, Z),  \
-U_NP,                 U_NP,           U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPC),   U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET), U_LT(U_NUM, BSPC),  U_LT(U_FUN, DEL),   U_NP,             U_NP
-
 #define MIRYOKU_LAYER_BASE \
 &kp P,             &td1,             &kp DE_MINS,        &kp DE_COMM,        &kp Q,              &kp G,             &kp C,             &kp L,              &kp M,            &kp F,              \
 U_MT(LGUI, H),     U_MT(LALT, I),     U_MT(LCTRL, E),     &td4,     &td2,              &kp D,             U_MT(LSHFT, T),    U_MT(LCTRL, R),     U_MT(LALT, N),    &td3,      \
 U_LT(U_BUTTON, K), U_MT(RALT, DE_Y),     &kp DE_DOT,         &kp DE_QUOT,        &kp X,              &kp J,             &kp V,             &kp W,              U_MT(RALT, B),    U_LT(U_BUTTON, DE_Z),  \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),   U_NP,             U_NP
+
+#define MIRYOKU_LAYER_NUM \
+&kp DE_LBRC,       &kp N7,            &kp N8,            &kp N9,            &kp DE_RBRC,       U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp DE_SCLN,       &kp N4,            &kp N5,            &kp N6,            &kp DE_EQL,        U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp DE_ACUT,       &kp N1,            &kp N2,            &kp N3,            &kp DE_BSLS,       U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp DE_MINS,       U_NA,              U_NA,              U_NA,              U_NP,              U_NP
