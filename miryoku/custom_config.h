0e9eac85af2782c8c1cb2a298aba484c9aaa2501
KohlRaphael <raphael.kohl@charite.de>
2024-02-23 11:33:16 +0100

    change to poq

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -10,9 +10,9 @@
 //U_NP,                 U_NP,           U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPC),   U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET), U_LT(U_NUM, BSPC),  U_LT(U_FUN, DEL),   U_NP,             U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
-&kp Q,                &kp W,          &kp E,              &kp R,              &kp T,            &kp DE_Z,         &kp U,              &kp I,              &kp O,            &kp P,                  \
-U_MT(LGUI, A),        U_MT(LALT, S),  U_MT(LCTRL, D),     U_MT(LSHFT, F),     &kp G,              &kp H,            U_MT(LSHFT, J),     U_MT(LCTRL, K),     U_MT(LALT, L),    U_MT(LGUI, SQT),        \
-U_LT(U_BUTTON, DE_Y), U_MT(RALT, X),  &kp C,              &kp V,              &kp B,              &kp N,            &kp M,              &kp COMMA,          U_MT(RALT, DOT),  U_LT(U_BUTTON, SLASH),  \
+&kp P,                &kp O,          &kp DE_MINS,        &kp DE_COMM,        &kp Q,            &kp G,            &kp C,              &kp L,              &kp M,            &kp F,                  \
+U_MT(LGUI, H),        U_MT(LALT, I),  U_MT(LCTRL, E),     U_MT(LSHFT, A),     &kp O,              &kp D,            U_MT(LSHFT, T),     U_MT(LCTRL, R),     U_MT(LALT, N),    U_MT(LGUI, S),        \
+U_LT(U_BUTTON, K),    U_MT(RALT, Y),  &kp DE_DOT,         &kp DE_QUOT,        &kp X,              &kp J,            &kp V,              &kp W,              U_MT(RALT, B),    U_LT(U_BUTTON, Z),  \
 U_NP,                 U_NP,           U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPC),   U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET), U_LT(U_NUM, BSPC),  U_LT(U_FUN, DEL),   U_NP,             U_NP
 
 #define MIRYOKU_ALTERNATIVES_NUM \
