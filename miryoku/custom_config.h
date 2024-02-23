e25614fb2b126d9f5900cad0d7b68695441754c8
KohlRaphael <raphael.kohl@charite.de>
2024-02-23 16:36:57 +0100

    add tap-dance A Ä & minor changes

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -17,6 +17,6 @@ U_NP,                 U_NP,           U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPC),   U_
 
 #define MIRYOKU_LAYER_BASE \
 &kp P,             &td1,             &kp DE_MINS,        &kp DE_COMM,        &kp Q,              &kp G,             &kp C,             &kp L,              &kp M,            &kp F,              \
-U_MT(LGUI, H),     U_MT(LALT, I),     U_MT(LCTRL, E),     U_MT(LSHFT, A),     &td2,              &kp D,             U_MT(LSHFT, T),    U_MT(LCTRL, R),     U_MT(LALT, N),    &td3,      \
+U_MT(LGUI, H),     U_MT(LALT, I),     U_MT(LCTRL, E),     &td4,     &td2,              &kp D,             U_MT(LSHFT, T),    U_MT(LCTRL, R),     U_MT(LALT, N),    &td3,      \
 U_LT(U_BUTTON, K), U_MT(RALT, DE_Y),     &kp DE_DOT,         &kp DE_QUOT,        &kp X,              &kp J,             &kp V,             &kp W,              U_MT(RALT, B),    U_LT(U_BUTTON, DE_Z),  \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),   U_NP,             U_NP
